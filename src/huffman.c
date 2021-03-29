0fe5b1063c35cac4fed71268baa28ec00564ce2e
jazielloureiro <jazielloureiro17@gmail.com>
2021-03-29 17:06:34 -0300

    Add basics file manipulation functions

---
+++ src/huffman.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *init_file(char *filename){
+	FILE *file = fopen(filename, "r");
+
+	if(file == NULL){
+		printf("It wasn't possible to read %s", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	return file;
+}
+
+long get_file_length(FILE *file){
+	long length;
+
+	fseek(file, 0, SEEK_END);
+	length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	return length;
+}
+
+char *read_whole_file(char *filename){
+	FILE *file = init_file(filename);
+	long length = get_file_length(file);
+	char *buffer = malloc(length);
+
+	fread(buffer, 1, length, file);
+
+	return buffer;
+}
+
+int main(){
+	puts(read_whole_file("data/input1.txt"));
+	
+	return 0;
+}
